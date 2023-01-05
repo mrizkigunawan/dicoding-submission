5a0a3a3e44c61205cf089f9979c8ddec5ffffa23
mrizkigunawan <rizkigunawan300@gmail.com>
2023-01-05 10:44:46 +0800

    Fix: accessibility

--- src/scripts/views/app.js
+++ src/scripts/views/app.js
@@ -30,6 +30,11 @@ class App {
     const page = routes[url];
     this._content.innerHTML = await page.render();
     await page.afterRender();
+    const skipLinkElem = document.querySelector('.skip-content');
+    skipLinkElem.addEventListener('click', (event) => {
+      event.preventDefault();
+      document.querySelector('.main').focus();
+    });
   }
 }
 
